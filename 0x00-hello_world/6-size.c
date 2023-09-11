912a037fe716909e250abf268075b3eacbfb22b2
Faith <oyamafaith9@gmail.com>
2023-09-11 09:56:20 -0700

    print types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("Size of a char: %nm byte(s)", sizeof(char));
 printf("Size of an int: %nm bytes(s)", sizeof(int));
 printf("Size of an long int: %nm bytes(s)", sizeof(long int));
 printf("Size of a long long int: %nm bytes(s)", sizeof(long long int));
-printf("Size of a float: %nm bytes(s)", sizeof(float);
+ printf("Size of a float: %nm bytes(s)", sizeof(float));
 return (0);
 }

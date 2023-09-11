b9ebfe1943e452cdf4505928373d9f0f313632d9
Faith <oyamafaith9@gmail.com>
2023-09-11 13:25:14 -0700

    print data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of a char: %u byte(s)", sizeof(char));
-printf("Size of an int: %u bytes(s)", sizeof(int));
-printf("Size of an long int: %u bytes(s)", sizeof(long int));
-printf("Size of a long long int: %u bytes(s)", sizeof(long long int));
-printf("Size of a float: %u bytes(s)", sizeof(float));
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of an int: %lu bytes(s)", sizeof(int));
+printf("Size of an long int: %lu bytes(s)", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
+printf("Size of a float: %lu bytes(s)", sizeof(float));
 return (0);
 }

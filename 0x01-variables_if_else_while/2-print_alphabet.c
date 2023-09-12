ed115cf6af047920759b9b6f885394fbeb523ab6
Faith <oyamafaith9@gmail.com>
2023-09-12 14:23:09 +0100

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function or entry point
+ * Return: Returns 0 when succesful
+ */
+int main(void)
+{
+char i = 'a';
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}

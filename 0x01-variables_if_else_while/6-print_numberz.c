8481c96ed8af2eca62464ebc4805ae60abfbbd84
Faith <oyamafaith9@gmail.com>
2023-09-12 19:49:10 +0100

    prints all single digit numbers 2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This program prints all single digit number of base 10
+ * Return: Returns 0 when program is succesful
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 0 ; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}

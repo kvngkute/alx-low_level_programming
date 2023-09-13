c1f6ec6fea54680478c1ae8c9085f9036e4c4ef0
Faith <oyamafaith9@gmail.com>
2023-09-14 00:10:20 +0100

    print single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,13 @@
  * main - This program prints all the possible combination of single-digits
  * Return: Returns 0 when succesful
  */
-int main()
+int main(void)
 {
-int i, j;
+int i;
 for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-if (j != 0)
 {
 putchar(',');
 putchar(' ');
-}
-putchar((i % 10) + '0');
-}
 }
 return (0);
 }

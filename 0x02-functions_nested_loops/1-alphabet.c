024435d55d9e86f218b4fabcf3f64a05a329f16a
Faith <oyamafaith9@gmail.com>
2023-09-14 14:00:25 +0100

    prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints all the alphabets
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}

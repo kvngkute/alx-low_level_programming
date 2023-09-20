70ca191167a20d0e90b51f40fa34536aed5bd975
Faith <oyamafaith9@gmail.com>
2023-09-20 22:45:04 +0100

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,10 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != s2[i])
 	{
 		if (s1[i] != s2[i]
-				{
-				return (s1[i] - s2[i]);)
-						}
-						i++;
-						}
-						return (0);
-						}
+		{
+			return (s1[i] - s2[i]);)
+		}
+		i++;
+	}
+	return (0);
+}

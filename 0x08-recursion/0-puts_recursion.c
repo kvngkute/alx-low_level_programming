62f609d24b6859707845f2116ea8acef69e833f6
Faith <oyamafaith9@gmail.com>
2023-09-29 01:32:34 +0100

    task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * _puts_recursion - Prints a string followed by a new line
- * @s: Input
- * Return: Always 0 
+ * @s: input
+ *
+ * Return: Always 0
  */
 void _puts_recursion(char *s)
 {

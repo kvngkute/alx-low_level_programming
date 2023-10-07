5354981ee49cd766036c22772e3ea2e7b6abff33
Faith <oyamafaith9@gmail.com>
2023-10-07 20:52:53 +0100

    arguments to main task 1 and 2

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include 'main.h'
+
+/**
+ * main - Prints name of the program
+ * @argc: number of argument
+ * @argv: array of argument
+ *
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
